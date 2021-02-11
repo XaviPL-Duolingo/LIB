4ee04847f68561994e18699a68bdf60711017152
Xavi-PL <xavipl.mvk25@gmail.com>
2021-02-11 15:49:25 +0100

    MainWindow al 90%

--- src/main/java/com/duolingo/app/model/Course.java
+++ src/main/java/com/duolingo/app/model/Course.java
@@ -59,6 +59,6 @@ public class Course {
 
 	@Override
 	public String toString() {
-		return idOriginLang.getNameLanguage() + " == " + idDestLang.getNameLanguage();
+		return idOriginLang.getNameLanguage() + " → " + idDestLang.getNameLanguage();
 	}
 }

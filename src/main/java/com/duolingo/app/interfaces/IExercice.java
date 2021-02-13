7bd763d2cfdbe11a0ea898aa605b0c99d09bc36e
Xavi-PL <xavipl.mvk25@gmail.com>
2021-02-13 12:37:52 +0100

    + TypeExerciceWindows Tipo test listisimo!

--- src/main/java/com/duolingo/app/interfaces/IExercice.java
+++ src/main/java/com/duolingo/app/interfaces/IExercice.java
@@ -1,5 +1,15 @@
 package com.duolingo.app.interfaces;
 
+import com.duolingo.app.model.Exercice;
+
+import java.util.List;
+
 public interface IExercice {
+
+    public void createExercice(int idLevel, int idTypeLevel, String[] contentExercice);
+
+    public List<Exercice> getAllExercicesByLevelID(int idLevel);
+
+    public Exercice getExerciceByID(int idExercice);
 
 }
--- src/main/java/com/duolingo/app/interfaces/impl/ExerciceImpl.java
+++ src/main/java/com/duolingo/app/interfaces/impl/ExerciceImpl.java
@@ -1,7 +1,24 @@
 package com.duolingo.app.interfaces.impl;
 
 import com.duolingo.app.interfaces.IExercice;
+import com.duolingo.app.model.Exercice;
+
+import java.util.List;
 
 public class ExerciceImpl implements IExercice{
 
+    @Override
+    public void createExercice(int idLevel, int idTypeLevel, String[] contentExercice) {
+
+    }
+
+    @Override
+    public List<Exercice> getAllExercicesByLevelID(int idLevel) {
+        return null;
+    }
+
+    @Override
+    public Exercice getExerciceByID(int idExercice) {
+        return null;
+    }
 }

b5b936c9bf9b366913c8f8e8e7c19a8ff02f309a
Xavi-PL <xavipl.mvk25@gmail.com>
2021-02-14 00:25:58 +0100

    + TypeExerciceWindows Layouts de los ejercicios creados!

--- src/main/java/com/duolingo/app/interfaces/impl/ExerciceImpl.java
+++ src/main/java/com/duolingo/app/interfaces/impl/ExerciceImpl.java
@@ -2,6 +2,9 @@ package com.duolingo.app.interfaces.impl;
 
 import com.duolingo.app.interfaces.IExercice;
 import com.duolingo.app.model.Exercice;
+import com.duolingo.app.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -9,6 +12,17 @@ public class ExerciceImpl implements IExercice{
 
     @Override
     public void createExercice(int idLevel, int idTypeLevel, String[] contentExercice) {
+
+        Transaction t = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            t = session.beginTransaction();
+            // session.save(ex);
+            t.commit();
+            System.out.println("Insertado correctamente!");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 
